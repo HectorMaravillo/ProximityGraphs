c5c992d97a8c257c5f36889bdadd7cb4519e1e02
Héctor Maravillo <hector.maravillo@im.unam.mx>
2025-08-21 20:13:48 -0600

    add parameter rng in points_on_sphere

--- proximitygraphs/utils.py
+++ proximitygraphs/utils.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-def points_on_sphere(n, dims, seed=73):
+def points_on_sphere(n, dims, seed=73, rng=None):
     #https://mathworld.wolfram.com/SpherePointPicking.html
     #https://math.stackexchange.com/questions/444700/uniform-distribution-on-the-surface-of-unit-sphere
-    rng = np.random.default_rng(seed)
+    if rng is None:
+        rng = np.random.default_rng(seed)
     X = rng.normal(size=(n, dims))
     X_norm = np.linalg.norm(X, axis=1, keepdims=True)
     Y = X / X_norm
