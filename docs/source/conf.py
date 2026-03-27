c6ccc0364ebfd78a63cdd5d24b9990585acf7f56
Diego Villarreal <diegovillarreal354@gmail.com>
2026-03-26 21:45:37 -0600

    fix #4 ruff formater

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,66 +8,65 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..')) 
 
-project = 'Proximity Graphs'
-copyright = '2025, Héctor Maravillo'
-author = 'Héctor Maravillo , Diego Villarreal , Heriberto Espino'
-release = 'v.0.1'
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "Proximity Graphs"
+copyright = "2025, Héctor Maravillo"
+author = "Héctor Maravillo , Diego Villarreal , Heriberto Espino"
+release = "v.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["myst_parser",
+extensions = [
+    "myst_parser",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.mathjax",
-    "sphinx_autodoc_typehints",]
+    "sphinx_autodoc_typehints",
+]
 
 myst_enable_extensions = [
-    "deflist", "colon_fence", "dollarmath", "amsmath", "html_image"
+    "deflist",
+    "colon_fence",
+    "dollarmath",
+    "amsmath",
+    "html_image",
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".md": "markdown",
 }
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
 
 html_css_files = [
-    'custom.css',
+    "custom.css",
 ]
 
 html_theme_options = {
-    
-  
     "home_page_in_toc": True,
     "show_navbar_depth": 1,
     "navigation_with_keys": True,
-    
-  
+    "use_download_button": False,
     "toc_title": "En esta página",
     "show_toc_level": 1,
-    
-   
-    "show_prev_next": True,             
-    
+    "show_prev_next": True,
     "search_bar_text": "Buscar...",
-    
     # Logo (opcional)
     # "logo": {
     #     "image_light": "_static/logo-light.png",
     #     "image_dark": "_static/logo-dark.png",
     # },
-    
     # Syntax highlighting
     "pygments_light_style": "tango",
     "pygments_dark_style": "monokai",
@@ -76,6 +75,4 @@ html_theme_options = {
 html_title = "Proximity Graphs Documentation"
 
 
-html_context = {
-    "default_mode": "light"
-}
+html_context = {"default_mode": "light"}
